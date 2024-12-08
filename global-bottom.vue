475914d95eefb51f4cac14e2eb1123bc930e96f7
amorales <ajmoralesartacho@gmail.com>
2024-12-08 22:35:15 +0100

    add patyh

--- global-bottom.vue
+++ global-bottom.vue
@@ -5,8 +5,8 @@
   >
     <div class="flex justify-between">
       <!-- <p class="text-gray-500">Antonio J. Morales</p> -->
-      <img src="/insep_logo.png" width="60" />
-      <img src="/fftri_logo.svg" width="60" />
+      <img src="./insep_logo.png" width="60" />
+      <img src="./fftri_logo.svg" width="60" />
     </div>
   </footer>
 </template>
